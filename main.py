a7983988ecb10a53d56cd838bb3ea89b1d79e8d6
michelangelo7 <m_che_l@hotmail.com>
2020-09-10 23:31:53 -0300

    Test create User and Expense

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 from datetime import datetime
 from expenses import Expenses
+from user import User
 
 def main():
-  newUser = ("Michel", 31)
+  newUser = User("Michel", 31)
+  newExpense = Expenses("Mercado livre", -150)
   
   print(newUser)
+  print(newExpense)
+
+  newUser.register_expenses(newExpense)
+
+
+
 
 if __name__ == "__main__":
     main()
