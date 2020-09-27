3db9e6235e1909f7d2c0d6181349e132b70900d6
michelangelo7 <m_che_l@hotmail.com>
2020-09-27 10:31:54 -0300

    Test class Credit Card

--- user/user.py
+++ user/user.py
@@ -1,9 +1,17 @@
 from people import People
+from credit_card import CreditCard
 
 class User(People):
   def __init__(self, name, age):
     super().__init__(name, age)
     self.expenses = []
+
+class CreditCard(CreditCard):
+  def __init__(self, dateOfPurchase, creditorCompany=None,
+                 numberOfInstallments=0, InstallmentsValue=0, category=None):
+      super().__init__(dateOfPurchase=0, creditorCompany=None, 
+      numberOfInstallments=0, InstallmentsValue=0, category=None)              
+      self.creadCard = []
 
   def __str__(self):
     return f'Nome: {self.name}, Idade: {self.age}'
